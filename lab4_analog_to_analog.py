fb5bfa186caecac628144f37f5ba49573dbd765d
Arpita saha Chaity <arpitasahachaity49@gmail.com>
2026-05-10 23:33:45 +0600

    lab4_analog_to_analog.py

--- lab4_analog_to_analog.py
+++ lab4_analog_to_analog.py
@@ -2,66 +2,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # User Inputs
+Am = int(input("Enter message amplitude: "))
+Ac = int(input("Enter carrier amplitude: "))
+
 fm = int(input("Enter message frequency (Hz): "))
 fc = int(input("Enter carrier frequency (Hz): "))
 
+kp = int(input("Enter phase sensitivity: "))
+
 # Time axis
-t = np.linspace(0, 1, 1000)
+t = np.linspace(0, 1, 2000)
 
-# Message Signal
-message = np.sin(2 * np.pi * fm * t)
+# Message and Carrier Signal
+message = Am * np.sin(2 * np.pi * fm * t)
 
-# Carrier Signal
-carrier = np.sin(2 * np.pi * fc * t)
+carrier = Ac * np.sin(2 * np.pi * fc * t)
 
-# AM Modulation
+# AM
 am = (1 + message) * carrier
 
-# FM Modulation
-kf = 80
+# FM
+kf = 50
 fm_signal = np.sin(
     2 * np.pi * fc * t +
-    kf * np.cumsum(message) / 100
+    kf * np.cumsum(message) / len(t)
 )
 
-# PM Modulation
-kp = 10
+# PM
 pm_signal = np.sin(
     2 * np.pi * fc * t +
     kp * message
 )
 
 # Plotting
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(10, 10))
 
-# Message Signal
-plt.subplot(4, 1, 1)
+plt.subplot(5, 1, 1)
 plt.plot(t, message)
 plt.title("Message Signal")
 plt.grid()
 
-# AM Signal
-plt.subplot(4, 1, 2)
+plt.subplot(5, 1, 2)
+plt.plot(t, carrier)
+plt.title("Carrier Signal")
+plt.grid()
+
+plt.subplot(5, 1, 3)
 plt.plot(t, am)
 plt.title("Amplitude Modulation (AM)")
 plt.grid()
 
-# FM Signal
-plt.subplot(4, 1, 3)
+plt.subplot(5, 1, 4)
 plt.plot(t, fm_signal)
 plt.title("Frequency Modulation (FM)")
 plt.grid()
 
-# PM Signal
-plt.subplot(4, 1, 4)
-plt.plot(t, pm_signal)
+plt.subplot(5, 1, 5)
+plt.plot(t, carrier, label="Carrier")
+plt.plot(t, pm_signal, label="PM Signal")
 plt.title("Phase Modulation (PM)")
+plt.legend()
 plt.grid()
 
-# Zoom for clear view
-plt.xlim(0, 0.3)
+plt.xlim(0, 0.2)
 
 plt.tight_layout()
 plt.show()
 
-#Sample input 2 20
+## sample input
+## amplitude 5,5
+## frequency 2 20
+## phase 10
